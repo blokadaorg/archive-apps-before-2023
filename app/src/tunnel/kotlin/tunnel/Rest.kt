c1ab4c9bee1f0493fe153c8e14bba2ea213ec9d0
kar <hi@kar.gs>
2019-05-31 10:15:15 +0200

    rewrite tunnel for performance

--- app/src/tunnel/kotlin/tunnel/Rest.kt
+++ app/src/tunnel/kotlin/tunnel/Rest.kt
@@ -1,6 +1,7 @@
 package tunnel
 
 import android.content.Context
+import android.os.Build
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.instance
@@ -9,6 +10,7 @@ import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
 import okhttp3.OkHttpClient
+import org.blokada.BuildConfig
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -96,6 +98,19 @@ fun newRestApiModule(ctx: Context): Kodein.Module {
                         chain.connection()?.socket()?.let {
                             tun.protect(it)
                         }
+                        chain.proceed(request)
+                    }
+                    .addInterceptor { chain ->
+                        val request = chain.request().newBuilder().header("User-Agent",
+                                "blokada/%s (android-%s %s %s %s %s-%s)".format(
+                                        BuildConfig.VERSION_NAME,
+                                        Build.VERSION.SDK_INT,
+                                        BuildConfig.FLAVOR,
+                                        BuildConfig.BUILD_TYPE,
+                                        Build.SUPPORTED_ABIS[0],
+                                        Build.MANUFACTURER,
+                                        Build.DEVICE
+                                )).build()
                         chain.proceed(request)
                     }
                     .build()
